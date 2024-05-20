e16e1e072dc89c2c23c6a89bde99ae85e432c2ea
Ricardo <ricardo.alvarez@thoughtworks.com>
2024-05-20 02:48:28 -0400

    Creating lambda functions and api gateway, uploading py files to s3 with module

---
+++ api_gw2.tf
@@ -0,0 +1,83 @@
+resource "aws_apigatewayv2_api" "api_gw_lambda" {
+  name                     = format("%s-api-gw-lambda", var.prefix)
+  protocol_type            = "HTTP"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw_log_group" {
+  name = format("%s-api-gw-log", var.prefix)
+}
+
+resource "aws_apigatewayv2_stage" "apigw2_stage" {
+  api_id = aws_apigatewayv2_api.api_gw_lambda.id
+  name   = "$default"
+  auto_deploy = true
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw_log_group.arn
+    format = jsonencode({
+      "requestId" : "$context.requestId",
+      "extendedRequestId" : "$context.extendedRequestId",
+      "ip" : "$context.identity.sourceIp",
+      "caller" : "$context.identity.caller",
+      "user" : "$context.identity.user",
+      "requestTime" : "$context.requestTime",
+      "httpMethod" : "$context.httpMethod",
+      "resourcePath" : "$context.resourcePath",
+      "status" : "$context.status",
+      "protocol" : "$context.protocol",
+      "responseLength" : "$context.responseLength",
+      "integrationErrorMessage" : "$context.integrationErrorMessage",
+      "errorMessage" : "$context.error.message",
+      "errorResponseType" : "$context.error.responseType"
+    })
+  }
+}
+
+locals {
+	lambda_functions = [
+		module.lambda_user_registry,
+		module.lambda_user_verify
+	]
+	api_gw_route_keys = [
+		"GET /register", "GET /"
+	]
+}
+
+resource "aws_apigatewayv2_integration" "api_gw2_integrations" {
+  count = 2
+  api_id             = aws_apigatewayv2_api.api_gw_lambda.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = local.lambda_functions[count.index].lambda_function_invoke_arn
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "api_gw2_routes" {
+	count 	  = 2
+  api_id    = aws_apigatewayv2_api.api_gw_lambda.id
+  route_key = local.api_gw_route_keys[count.index]
+  target    = "integrations/${aws_apigatewayv2_integration.api_gw2_integrations[count.index].id}"
+}
+
+resource "aws_apigatewayv2_deployment" "api_gw2_lambda_deployments" {
+	count = 2
+  api_id      = aws_apigatewayv2_api.api_gw_lambda.id
+  description = "Deployment of apigateway v2"
+  triggers = {
+    redeployment = sha1(join(",", tolist([
+        jsonencode(aws_apigatewayv2_integration.api_gw2_integrations[count.index]), 
+        jsonencode(aws_apigatewayv2_route.api_gw2_routes[count.index]),
+      ]))
+    )
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lambda_permission" "apigw2_lambda_permission" {
+  count = 2
+	statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = local.lambda_functions[count.index].lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api_gw_lambda.execution_arn}/*/*"
+}
---
+++ lambda.tf
@@ -0,0 +1,100 @@
+module "lambda_user_registry" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = format("%s-users_register", var.prefix)
+  description   = "Lambda function for user registry"
+  handler       = "register_user.lambda_handler"
+  runtime       = "python3.10"
+
+  source_path = "./src/register_user.py"
+	publish                = true
+
+	environment_variables = {
+    DB_TABLE_NAME      = aws_dynamodb_table.users_table.name
+    Serverless = "Terraform"
+  }
+
+	cloudwatch_logs_log_group_class = "STANDARD"
+
+  cors = {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+
+  invoke_mode = "RESPONSE_STREAM"
+  attach_policy_statements = true
+	policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = [
+				"dynamodb:PutItem",
+        "dynamodb:UpdateItem",
+        "dynamodb:BatchWriteItem"
+			],
+      resources = [aws_dynamodb_table.users_table.arn]
+    }
+  }
+
+  tags = {
+    Name = "lambda_user_registry"
+  }
+}
+
+
+module "lambda_user_verify" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = format("%s-users_verify", var.prefix)
+  description   = "Lambda function for user verify"
+  handler       = "verify_user.lambda_handler"
+  runtime       = "python3.10"
+
+  source_path = "./src/verify_user.py"
+	publish                = true
+
+	environment_variables = {
+    DB_TABLE_NAME      = aws_dynamodb_table.users_table.name
+    WEBSITE_S3         = aws_s3_bucket.website_assignment_bucket.id
+  }
+
+	cloudwatch_logs_log_group_class = "STANDARD"
+
+  cors = {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+
+  invoke_mode = "RESPONSE_STREAM"
+  attach_policy_statements = true
+	policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = [
+        "dynamodb:GetItem"
+			],
+      resources = [aws_dynamodb_table.users_table.arn]
+    },
+    s3_read = {
+      effect    = "Allow",
+      actions   = [
+				"s3:GetObject",
+        "s3:ListBucket"
+			],
+      resources = [
+				format("%s/*", aws_s3_bucket.website_assignment_bucket.arn)
+			]
+    }
+  }
+  tags = {
+    Name = "lambda_user_registry"
+  }
+}
+
